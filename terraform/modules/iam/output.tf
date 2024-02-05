768d915125e32d32621815cfc9718a0cf1b50c03
mirik12 <mirik12mirik@gmail.com>
2024-02-05 15:28:28 +0100

    make change in output.tf in iam module

--- terraform/modules/iam/output.tf
+++ terraform/modules/iam/output.tf
@@ -2,13 +2,3 @@ output "user_arn" {
   value       = aws_iam_user.user.arn
   description = "The ARN of the IAM user."
 }
-
-output "user_access_key_id" {
-  value       = aws_iam_access_key.user_key.id
-  description = "The access key ID for the IAM user."
-}
-
-output "user_secret_access_key" {
-  value       = aws_iam_access_key.user_key.secret
-  description = "The secret access key for the IAM user. This should be treated as sensitive information."
-}
