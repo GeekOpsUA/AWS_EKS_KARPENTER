c9ce9bc5f7630fa02d80c75d5c5d985fc4087e87
mirik12 <mirik12mirik@gmail.com>
2024-02-16 10:14:34 +0100

    change main and variables

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,33 +1,42 @@
-variable "user_name" {}
-
-variable "policy_statements" {}
-
-provider "aws" {
-  region = "eu-north-1" 
+variable "user_name" {
+  description = "Name of the IAM user"
 }
 
-resource "aws_iam_user" "user" {
-  name = var.user_name
-}
-
-data "aws_iam_policy_document" "user_policy" {
-  dynamic "statement" {
-    for_each = var.policy_statements
-
-    content {
-      actions   = statement.value.actions
-      resources = statement.value.resources
-      effect    = statement.value.effect
+variable "policy_statements" {
+  description = "Map of policy statements for dynamic policy creation"
+  type = list(object({
+    actions   = list(string)
+    resources = list(string)
+    effect    = string
+  }))
+  default = [
+    {
+      actions   = ["s3:ListBucket", "s3:GetObject", "s3:PutObject"],
+      resources = ["arn:aws:s3:::example_bucket", "arn:aws:s3:::example_bucket/*"],
+      effect    = "Allow"
+    },
+    {
+      actions   = ["ecr:GetDownloadUrlForLayer", "ecr:BatchGetImage", "ecr:BatchCheckLayerAvailability"],
+      resources = ["arn:aws:ecr:${var.aws_region}:*:*"],
+      effect    = "Allow"
+    },
+    {
+      actions   = ["eks:CreateCluster", "eks:DeleteCluster", "eks:DescribeCluster", "eks:ListClusters", "eks:UpdateClusterConfig", "eks:UpdateClusterVersion"],
+      resources = ["*"],
+      effect    = "Allow"
+    },
+    {
+      actions   = ["eks:CreateFargateProfile", "eks:DeleteFargateProfile", "eks:DescribeFargateProfile", "eks:ListFargateProfiles"],
+      resources = ["*"],
+      effect    = "Allow"
     }
-  }
+  ]
 }
 
-resource "aws_iam_policy" "user_policy" {
-  name   = "${var.user_name}-policy"
-  policy = data.aws_iam_policy_document.user_policy.json
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket"
 }
 
-resource "aws_iam_user_policy_attachment" "user_policy_attachment" {
-  user       = aws_iam_user.user.name
-  policy_arn = aws_iam_policy.user_policy.arn
+variable "aws_region" {
+  description = "AWS region"
 }
--- terraform/modules/iam/variables.tf
+++ terraform/modules/iam/variables.tf
@@ -1,26 +1,9 @@
-variable "user_name" {
-  description = "Name of the IAM user to be created"
-  type        = string
-}
+provider "aws" {}
 
-variable "policy_statements" {
-  description = "Map of policy statements for dynamic policy creation"
-  type = list(object({
-    actions   = list(string)
-    resources = list(string)
-    effect    = string
-  }))
-  default = [
-    {
-      actions   = ["s3:ListBucket", "s3:GetObject", "s3:PutObject"],
-      resources = ["arn:aws:s3:::example_bucket", "arn:aws:s3:::example_bucket/*"],
-      effect    = "Allow"
-    },
-    {
-      actions   = ["ecr:GetDownloadUrlForLayer", "ecr:BatchGetImage"],
-      resources = ["arn:aws:ecr:us-east-1:123456789012:repository/example"],
-      effect    = "Allow"
-    }
-
-  ]
+module "iam" {
+  source         = "../modules/iam"
+  statement      = var.policy_statements
+  s3_bucket_name = var.s3_bucket_name
+  aws_region     = var.aws_region
+  user_name      = var.user_name
 }
